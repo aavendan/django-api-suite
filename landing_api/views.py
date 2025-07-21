c0ffe19ccf0ae42fb86a9206bff96c4f00a456e8
Allan <aavendan@gmail.com>
2025-07-21 01:15:39 +0000

    guia21

--- landing_api/views.py
+++ landing_api/views.py
@@ -24,3 +24,20 @@ class LandingAPI(APIView):
 
         # Devuelve un arreglo JSON
         return Response(data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+
+        data = request.data
+
+        # Referencia a la colección
+        ref = db.reference(f'{self.collection_name}')
+
+        current_time  = datetime.now()
+        custom_format = current_time.strftime("%d/%m/%Y, %I:%M:%S %p").lower().replace('am', 'a. m.').replace('pm', 'p. m.')
+        data.update({"timestamp": custom_format })
+
+        # push: Guarda el objeto en la colección
+        new_resource = ref.push(data)
+
+        # Devuelve el id del objeto guardado
+        return Response({"id": new_resource.key}, status=status.HTTP_201_CREATED)
